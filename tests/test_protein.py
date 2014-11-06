a448da4502c0799d6dbb6b66d5febeebd96af942
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-11-06 22:53:41 +0100

    bioinformatics_algorithms/protein/protein.py

--- tests/test_protein.py
+++ tests/test_protein.py
@@ -12,7 +12,6 @@ class TestProteinMethods(unittest.TestCase):
         res = 'MAMAPRTEINSTRING'
         self.assertEqual(res, ''.join(protein.rna_to_amino(rna)))
 
-
     def test_2_encoded_bt(self):
         """ Testing encoded_by method...
         """
@@ -20,3 +19,12 @@ class TestProteinMethods(unittest.TestCase):
         amino = 'MA'
         res = ['ATGGCC', 'ATGGCC', 'GGCCAT']
         self.assertEqual(res, protein.encoded_by(dna, amino))
+
+    def test_3_spectrum(self):
+        """ Testing mass spectrum calculation methods...
+        """
+        peptide = 'LEQN'
+        linear = [0, 113, 114, 128, 129, 242, 242, 257, 370, 371, 484]
+        cyclic = [0, 113, 114, 128, 129, 227, 242, 242, 257, 355, 356, 370, 371, 484]
+        self.assertEqual(protein.spectrum(peptide), linear)
+        self.assertEqual(protein.spectrum(peptide, cyclic=True), cyclic)
